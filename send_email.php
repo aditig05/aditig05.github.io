6759498f2b2bca9e3862616a3575bfac8fbc5267
Aditi Gupta <aditig0501@gmail.com>
2024-11-27 21:24:06 +0530

    editing index.html

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "aditig0501@gmail.com";  // Your email address
+    $subject = "New Contact Form Submission";
+    $message = "Name: " . $_POST['name'] . "\n";
+    $message .= "Email: " . $_POST['email'] . "\n";
+    $message .= "Phone: " . $_POST['phone'] . "\n";
+    $message .= "Message: " . $_POST['message'] . "\n";
+
+    $headers = "From: " . $_POST['email'];
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
