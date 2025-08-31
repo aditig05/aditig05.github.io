b1143b8c62662d4babcac37d332f0f4757183bfe
aditig05 <aditi5gupta2002@gmail.com>
2025-08-31 18:54:47 +0530

    resume modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,7 +52,7 @@ navToggleBtn.addEventListener("click", function () {
 document.getElementById('downloadcv').addEventListener('click', function() {
   var link = document.createElement('a');
   link.href = '/assets/Resume_Aditi.pdf; // Replace with the actual path to your PDF file
-  link.download = 'Aditi_Resume.pdf'; // Replace with the desired name for your downloaded PDF file
+  link.download = 'Resume_Aditi.pdf'; // Replace with the desired name for your downloaded PDF file
   console.log("Clicked")
   document.body.appendChild(link);
   link.click();
